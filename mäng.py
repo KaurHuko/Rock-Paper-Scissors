6783a97e632f3da0ff815e3b6b095d5d43168cd0
Kaur Huko Käämbre <kaurh.kaambre@gmail.com>
2024-11-13 23:16:48 +0200

    collisions

--- mäng.py
+++ mäng.py
@@ -1,28 +1,49 @@
+################################################
+# Programmeerimine I
+# 2024/2025 sügissemester
+#
+# Projekt
+# Teema: Kivi-Paber-Käärid simulatsioon
+#
+# Autorid: Maria Jansons, Kaur Huko Käämbre
+#
+# mõningane eeskuju: https://rock0n.itch.io/rock-paper-scissors-simulator
+##################################################
+
 import pygame, sys
 from pygame.locals import *
 from random import *
 
-pygame.init()
- 
+VALGE = (255, 255, 255)
+
 FPS = 30
-FramePerSec = pygame.time.Clock()
+SCREEN_WIDTH = 400
+SCREEN_HEIGHT = 600
 
-valge = (255, 255, 255)
-
-object_types = [
+OBJECT_COUNT = 8
+OBJECT_TYPES = [
     ("rock", "assets/rock.png"),
     ("paper", "assets/paper.png"),
     ("scissors", "assets/scissors.png")
 ]
 
-SCREEN_WIDTH = 400
-SCREEN_HEIGHT = 600
- 
-DISPLAYSURF = pygame.display.set_mode((400,600))
-DISPLAYSURF.fill(valge)
-pygame.display.set_caption("rock-paper-scissors")
+pygame.init()
+DISPLAYSURF = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+game_objects = []
 
-class GameObject(pygame.sprite.Sprite):
+
+def is_type1_weaker(type1, type2): # Muuda nõrgem tüüp tugevamaks, kui tüübid on erinevad
+    if (type1[0] == "rock" and type2[0] == "scissors"): return (type1, type1)
+    if (type1[0] == "scissors" and type2[0] == "paper"): return (type1, type1)
+    if (type1[0] == "paper" and type2[0] == "rock"): return (type1, type1)
+    
+    if (type2[0] == "rock" and type1[0] == "scissors"): return (type2, type2)
+    if (type2[0] == "scissors" and type1[0] == "paper"): return (type2, type2)
+    if (type2[0] == "paper" and type1[0] == "rock"): return (type2, type2)
+    
+    return (type1, type2)
+
+class GameObject(pygame.sprite.Sprite): # Game objekt on kas kivi/paber/käärid
 
     def __init__(self, type):
         super().__init__()
@@ -41,6 +62,8 @@ class GameObject(pygame.sprite.Sprite):
     
     def tick(self):
         self.move()
+        self.collide_with_others()
+        self.draw()
 
     def move(self):
         # Muuda kiirust vähehaaval, et sujuvam liikumine
@@ -53,36 +76,60 @@ class GameObject(pygame.sprite.Sprite):
         self.dy = max(-3, min(self.dy, 3))
 
         # Kontrolli piire ja põrka tagasi, kui on servas
-        if self.rect.top <= 0 or self.rect.bottom >= SCREEN_HEIGHT:
+        if self.rect.top < 0 or self.rect.bottom > SCREEN_HEIGHT:
             self.dy *= -1
-        if self.rect.left <= 0 or self.rect.right >= SCREEN_WIDTH:
+        if self.rect.left < 0 or self.rect.right > SCREEN_WIDTH:
             self.dx *= -1
         
         # Uuenda positsiooni
         self.rect.move_ip(self.dx, self.dy)
+        
+    def collide_with_others(self):
+        for other in game_objects:
+            if (not pygame.sprite.collide_rect(self, other)):
+                continue
+            
+            if (self.is_weaker_than(other)):
+                self.set_type(other.type)
+    
+    def is_weaker_than(self, other):
+        self_type = self.type[0]
+        other_type = other.type[0]
+        
+        if (other_type == "rock" and self_type == "scissors"): return True
+        if (other_type == "scissors" and self_type == "paper"): return True
+        if (other_type == "paper" and self_type == "rock"): return True
+        
+        return False
+        
+    def draw(self):
+        DISPLAYSURF.blit(self.image, self.rect)
 
-    def draw(self, surface):
-        surface.blit(self.image, self.rect)
-
-rock = GameObject(object_types[0])
-paper = GameObject(object_types[1])
-scissors = GameObject(object_types[2])
- 
-while True:     
+def tick():
     for event in pygame.event.get():              
         if event.type == QUIT:
             pygame.quit()
             sys.exit()
 
+    DISPLAYSURF.fill(VALGE)
+    
     # Objekti liigutamine ja joonistamine
-    rock.tick()
-    paper.tick()
-    scissors.tick()
-     
-    DISPLAYSURF.fill(valge)
-    rock.draw(DISPLAYSURF)
-    paper.draw(DISPLAYSURF)
-    scissors.draw(DISPLAYSURF)
-         
+    for game_object in game_objects:
+        game_object.tick()
+        
     pygame.display.update()
-    FramePerSec.tick(FPS)
+
+def setup():
+    pygame.display.set_caption("rock-paper-scissors")
+    DISPLAYSURF.fill(VALGE)
+    
+    for type_id in range(3):
+        for i in range(OBJECT_COUNT):
+            new_object = GameObject(OBJECT_TYPES[type_id])
+            game_objects.append(new_object)
+    
+    while True:
+        tick()
+        pygame.time.Clock().tick(FPS)
+    
+setup()
