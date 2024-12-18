3a6a9fb193f0150bca3261dca1474782728f997a
Kaur Huko Käämbre <kaurh.kaambre@gmail.com>
2024-12-18 14:10:55 +0200

    aa

--- src/startmenu.py
+++ src/startmenu.py
@@ -10,9 +10,8 @@ def start_setup():
     print("a")
     shared.display_surf.fill(shared.VALTER_VALGE)
     
-    button = pygame.Rect(80, 300, 100, 50)
-    pygame.draw.rect(shared.display_surf, shared.GANDALF_HALL, button)
-    buttons.append((button, game_setup))
+    button = Button(80, 300, 100, 50)
+    buttons.append(button)
     
     shared.current_tick = start_tick
 
@@ -20,6 +19,6 @@ def start_tick(events):
     for event in events:
         if event.type != MOUSEBUTTONDOWN: continue
     
-        for (button, event_func) in buttons:
+        for button in buttons:
             if button.collidepoint(event.pos):
                 event_func()
