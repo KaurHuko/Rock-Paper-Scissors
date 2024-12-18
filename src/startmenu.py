6867787e6f7cae6f9be996358189ff375b06c215
Kaur Huko Käämbre <kaurh.kaambre@gmail.com>
2024-12-18 13:08:37 +0200

    a

---
+++ src/startmenu.py
@@ -0,0 +1,9 @@
+import pygame
+from pygame.locals import *
+import shared
+
+def start_setup():
+    pass
+
+def start_tick():
+    pass
---
+++ src/win.py
@@ -0,0 +1,9 @@
+import pygame
+from pygame.locals import *
+import shared
+
+def win_setup():
+    pass
+
+def win_tick():
+    pass
